faf9db04a83dc58ffd172f37f31469d04d293df3
Temirlan <bamblebee988@gmail.com>
2020-05-29 21:02:19 +0300

    add loading display

--- src/App.js
+++ src/App.js
@@ -12,16 +12,21 @@ function App() {
     fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${term}&image_type=photo`)
       .then((res) => res.json())
       .then((data) => setImages(data.hits))
+      .then(() => setIsLoading(false))
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
-        {images.map((image) => (
-          <ImageCard key={image.id} image={image} />
-        ))}
-      </div>
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading ...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
